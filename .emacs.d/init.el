e6773920304b78ca5d0dda564ebaa23a7a61d61b
deniandriancode <deniandriancode@gmail.com>
2022-08-10 18:42:17 +0700

    update init.el and add Nixos config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,15 @@
+;; =============================
+;; 
+;; Author : Deni Andrian Prayoga
+;;
+;; =============================
+;;
+;; Minimalist Emacs config with Evil mode, most of the configs are
+;; inspired from Doom Emacs (https://github.com/doomemacs/doomemacs)
+
+;; ===============================================
+;; PACKAGES
+;; ===============================================
 ;; adding melpa repository for installing packages
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
@@ -31,7 +43,14 @@
 	yaml-mode
 	json-mode))
 ;; to install, use M-x package-install-selected-packages
+;; ===========================
+;; NOTE
+;; ===========================
+;; after running package-install-selected-packages, run M-x all-the-icons-install-fonts
 
+;; =====================
+;; CONFIG
+;; =====================
 ;; display line number
 (when (version<="26.0.50" emacs-version )
   (global-display-line-numbers-mode))
@@ -39,23 +58,37 @@
 ;; custom key binding
 (global-set-key (kbd "C-x C-'") 'auto-complete-mode)
 (global-set-key (kbd "C-x C-<return>") 'treemacs)
+(global-set-key (kbd "C-x C-\]") 'evil-mode)
 
 ;; doom mode line
 (doom-modeline-mode 1)
+
+;; adding directory to load-path
+(add-to-list 'load-path "~/.emacs.d/lisp")
 
 ;; emmet mode
 (add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
 (add-hook 'rjsx-mode-hook  'emmet-mode)
 
-;; enable evil mode (Emacs Vi Layer)
+;; =================================
+;; NOTE
+;; =================================
+;; the following config must be in order
+;; enable evil mode (Emacs Vi Layer) | +START_CONFIG
 (evil-mode 1)
-
 ;; evil custom key binding
 (define-key evil-normal-state-map (kbd "C-u") 'evil-scroll-up)
 (define-key evil-normal-state-map (kbd "C-r") 'undo-redo)
+;; | +END_CONFIG
 
+;; use either move-text of move-lines, note that to move multiple lines
+;; don't select using -- VISUAL LINE --, use -- VISUAL -- instead
 ;; move text default binding
 (move-text-default-bindings)
+
+;; move-lines package
+;; (require 'move-lines)
+;; (move-lines-binding)
 
 ;; treemacs customization
 (treemacs-resize-icons 15)
@@ -64,7 +97,7 @@
 ;; hilghlight current line
 (global-hl-line-mode)
 
-;; dashboard
+;; dashboard config
 (dashboard-setup-startup-hook)
 ;; Set the title
 (setq dashboard-banner-logo-title "I love Vim")
@@ -84,8 +117,11 @@
 (setq dashboard-set-navigator t)
 (setq dashboard-set-footer nil)
 
-;; multiple cursors
+;; multiple cursors | FIXME
 (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 
 ;; autopairs
 (electric-pair-mode 1)
@@ -105,6 +141,14 @@
 
 ;; disable cursor blink
 (setq blink-cursor-mode nil)
+
+;; ==========================
+;; EASY CUSTOMIZATION
+;; ==========================
+;; the following config generated from 'Easy Customization' in Emacs, you can delete it
+;; and replace with your own customization
+;; Setting Customized :
+;; * theme : doom-one
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
